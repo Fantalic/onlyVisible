dc9895a2985337071d59c48ffbc28cad5ad50f10
Wilke Oncken <won@eseebase.de>
2026-06-10 16:26:32 +0200

    clean up and fix of run script

--- run.bat
+++
@@ -1,5 +0,0 @@
-cd build
-g++ ../src/*.cpp -o Game.exe -O2 -Wall -Wno-missing-braces -I ../include/ -L ../lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
-IF %ERRORLEVEL% NEQ 0 pause /B %ERRORLEVEL%
-Game.exe
-cd..
--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
+
+case "$OSTYPE" in
+    msys*|cygwin*)
+        EXE=Game.exe
+        LIBS=(-lraylib -lopengl32 -lgdi32 -lwinmm)
+        ;;
+    *)
+        EXE=Game
+        LIBS=(-lraylib -lGL -lpthread -ldl -lX11 -lXrandr -lXi)
+        ;;
+esac
+
 cd build
-g++ ../src/*.cpp -o Game -O2 -Wall -Wno-missing-braces -I ../include/ -L ../lib/ -lraylib -lGL -lpthread -ldl -lX11 -lXrandr -lXi
-./Game
+g++ ../src/*.cpp -o "$EXE" -O2 -Wall -Wno-missing-braces -I ../include/ -L ../lib/ "${LIBS[@]}"
+"./$EXE"
 cd ..
